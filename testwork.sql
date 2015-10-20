df5f0883e0660784f7ecf512d7e7bc6e5906fe9e
angeluss angeluss <ddr88@list.ru>
2015-10-20 16:19:55 +0300

    add_db_dump

---
+++ testwork.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Час створення: Жов 20 2015 р., 16:18
+-- Версія сервера: 5.5.44-0ubuntu0.14.04.1
+-- Версія PHP: 5.5.9-1ubuntu4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База даних: `testwork`
+--
+CREATE DATABASE IF NOT EXISTS `testwork` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `testwork`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` int(3) NOT NULL,
+  `created_at` int(11) NOT NULL DEFAULT '0',
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Дамп даних таблиці `admin`
+--
+
+INSERT INTO `admin` (`id`, `login`, `password`, `email`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', 'admin@admin.com', 1, 1445339372, 1445339372);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `file`
+--
+
+CREATE TABLE IF NOT EXISTS `file` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `created_at` int(11) NOT NULL DEFAULT '0',
+  `updated_at` int(11) DEFAULT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `tbl_migration`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `tbl_migration`
+--
+
+INSERT INTO `tbl_migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1445330921),
+('m151020_084500_create_table_admins', 1445330922),
+('m151020_084510_create_table_file', 1445330922),
+('m151020_121310_add_title_to_file', 1445343248),
+('m151020_131551_add_admin', 1445347012);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
